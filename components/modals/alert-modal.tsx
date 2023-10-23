094492a090c75e90ede94fff87e8ecbc1268fad9
Abdulrahman Ayinde <ayindeabdulrahman16@gmail.com>
2023-10-23 17:11:28 +0000

    third commit

---
+++ components/modals/alert-modal.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Modal } from "@/components/ui/modal";
+import { Button } from "@/components/ui/button";
+
+interface AlertModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  loading: boolean;
+}
+
+export const AlertModal: React.FC<AlertModalProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  loading,
+}) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Are you sure"
+      description="This action can not be undone">
+      <div className="space-x-2 justify-end items-center flex pt-6 w-full">
+        <Button disabled={loading} variant={"outline"} onClick={onClose}>
+          Cancel
+        </Button>
+        <Button disabled={loading} variant={"destructive"} onClick={onConfirm}>
+          Confirm
+        </Button>
+      </div>
+    </Modal>
+  );
+};
