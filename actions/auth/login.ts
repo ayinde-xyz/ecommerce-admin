925d49481c6e6ef62fd284072eb0723900fd5744
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2025-12-14 15:04:49 +0100

    fixed login issues and added a toast

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -18,13 +18,18 @@ export async function signInEmailAction(data: z.infer<typeof loginFormSchema>) {
       asResponse: true,
     });
 
+    const result = await res.clone().json();
+
+    if (!res.ok) {
+      return result;
+    }
+
     if (res.ok) {
-      return { success: "Sign in successful" };
+      redirect("/");
     }
   } catch (err) {
     if (err instanceof APIError) {
       const errCode = err.body ? (err.body.code as ErrorCode) : "UNKNOWN";
-      console.dir(err, { depth: 5 });
       switch (errCode) {
         case "EMAIL_NOT_VERIFIED":
           redirect("/auth/verify?error=email_not_verified");
@@ -33,6 +38,6 @@ export async function signInEmailAction(data: z.infer<typeof loginFormSchema>) {
       }
     }
 
-    return { error: "Internal Server Error" };
+    throw err;
   }
 }
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -46,35 +46,13 @@ export function LoginForm({
   });
 
   const onSubmit = (data: z.infer<typeof loginFormSchema>) => {
-    // toast.loading("You submitted the following values:", {
-    //   description: (
-    //     <pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
-    //       <code>{JSON.stringify(data, null, 2)}</code>
-    //     </pre>
-    //   ),
-    //   position: "bottom-right",
-    //   classNames: {
-    //     content: "flex flex-col gap-2",
-    //   },
-    //   style: {
-    //     "--border-radius": "calc(var(--radius)  + 4px)",
-    //   } as React.CSSProperties,
-    // });
     startTransition(() =>
-      signInEmailAction(data)
-        .then((result) => {
-          console.log(error);
-          if (result?.error) {
-            toast.error(result.error);
-          }
-
-          if (result?.success) {
-            redirect("/");
-          }
-        })
-        .catch(() => {
-          toast.error("Something went wrong");
-        })
+      signInEmailAction(data).then((result) => {
+        console.log(result);
+        if (result?.code) {
+          toast.error(result.message || "Something went wrong");
+        }
+      })
     );
   };
 
