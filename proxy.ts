541ac5e325c0a6cd96ba6aa40176564b386426bc
ayinde-xyz <ayindeabdulrahman16@gmail.com>
2025-12-25 15:44:53 +0100

    edited the proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,26 +2,24 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSessionCookie } from "better-auth/cookies";
 
 export async function proxy(request: NextRequest) {
-  // const sessionCookie =
-  //   request.cookies.get("better-auth.session_token") ||
-  //   request.cookies.get("__Secure-better-auth.session_token");
+  const sessionCookie = getSessionCookie(request);
 
-  // const { pathname } = request.nextUrl;
+  const { pathname } = request.nextUrl;
+  console.log("Session Cookie in Middleware:", sessionCookie);
+  // Redirect authenticated users away from login/signup pages
+  if (sessionCookie && ["/auth/login", "/auth/signup"].includes(pathname)) {
+    return NextResponse.redirect(new URL("/dashboard/[storeId]", request.url));
+  }
 
-  // // Redirect unauthenticated users trying to access protected routes
-  // if (!sessionCookie && pathname.startsWith("/dashboard")) {
-  //   return NextResponse.redirect(new URL("/auth/signup", request.url));
-  // }
-  // console.log("Session Cookie in Middleware:", sessionCookie);
-  // // Redirect authenticated users away from login/signup pages
-  // if (sessionCookie && ["/auth/login", "/auth/signup"].includes(pathname)) {
-  //   return NextResponse.redirect(new URL("/dashboard/[storeId]", request.url));
-  // }
+  // Redirect unauthenticated users trying to access protected routes
+  if (!sessionCookie && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
 
   return NextResponse.next();
 }
 
 export const config = {
   // Apply middleware to these routes
-  matcher: ["/dashboard/:path*", "/auth/:path*"],
+  matcher: ["/dashboard", "/auth/:path*"],
 };
